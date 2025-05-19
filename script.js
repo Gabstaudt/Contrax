11276aa01be6bc26665e66c304166ac15a044fdc
Gabstaudt <macstaudt@gmail.com>
2025-05-19 09:34:43 -0300

    ajuste

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
   document.getElementById("loginForm").addEventListener("submit", function (e) {
-    e.preventDefault(); // Impede envio padrão do formulário
+    e.preventDefault(); 
 
     const email = document.getElementById("email").value;
     const senha = document.getElementById("senha").value;
 
     if (email === "gabimstaudt@gmail.com" && senha === "gabi123") {
-      window.location.href = "/inicial/inicial.html"; // redireciona para o dashboard
+      window.location.href = "/inicial/inicial.html"; 
     } else {
       alert("Email ou senha incorretos.");
     }
